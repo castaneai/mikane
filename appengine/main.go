fbafa628b2972f393a81ca55d928da3649aea9aa
castaneai <castaneai@castaneai.net>
2018-02-05 01:04:40 +0900

    Add total asset memcache

--- appengine/main.go
+++ appengine/main.go
@@ -9,6 +9,15 @@ import (
 	"context"
 	"google.golang.org/appengine/log"
 	"google.golang.org/appengine/urlfetch"
+	"google.golang.org/appengine/memcache"
+	"fmt"
+	"time"
+	"strconv"
+)
+
+const (
+	getTotalAssetCacheKey = "totalAsset"
+	cacheExpireSeconds = 86400
 )
 
 func init() {
@@ -24,21 +33,46 @@ func handleError(ctx context.Context, err error) {
 func getTotalAsset(c *gin.Context) {
 	ctx := appengine.NewContext(c.Request)
 
-	hc := urlfetch.Client(ctx)
-	opts := &mf.ClientOption{Host: "https://moneyforward.com", SessionID: os.Getenv("MF_SESSION")}
-	mfc, err := mf.NewClient(hc, opts)
-	if err != nil {
+	item, err := memcache.Get(ctx, getTotalAssetCacheKey)
+	if err != nil && err != memcache.ErrCacheMiss {
 		handleError(ctx, err)
 		return
 	}
 
-	totalAsset, err := mfc.GetTotalAsset(ctx)
+	if item == nil {
+		hc := urlfetch.Client(ctx)
+		opts := &mf.ClientOption{Host: "https://moneyforward.com", SessionID: os.Getenv("MF_SESSION")}
+		mfc, err := mf.NewClient(hc, opts)
+		if err != nil {
+			handleError(ctx, err)
+			return
+		}
+
+		totalAsset, err := mfc.GetTotalAsset(ctx)
+		if err != nil {
+			handleError(ctx, err)
+			return
+		}
+
+		item = &memcache.Item{
+			Key: getTotalAssetCacheKey,
+			Value: []byte(fmt.Sprintf("%d", totalAsset.Amount)),
+			Expiration: cacheExpireSeconds * time.Second,
+		}
+
+		if err := memcache.Set(ctx, item); err != nil {
+			handleError(ctx, err)
+			return
+		}
+	}
+
+	totalAmount, err := strconv.Atoi(string(item.Value))
 	if err != nil {
 		handleError(ctx, err)
 		return
 	}
 	c.JSON(http.StatusOK, gin.H{
 		"status": "OK",
-		"amount": totalAsset.Amount,
+		"amount": totalAmount,
 	})
 }
